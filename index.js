dc0a2f4dc2e9ac32504fb07c067dc575c29af25f
Farah Arar <farah02arar@gmail.com>
2023-10-02 12:03:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName="bob";
+
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+var upperCaseName = upperCaseCustomerName();
+
+
+function setBestCustomer() {
+   return bestCustomer = 'not bob';
+  }
+  
+  setBestCustomer();
+  console.log(bestCustomer);
+
+  var bestCustomer = 'not bob';
+
+function overwriteBestCustomer(newBestCustomer) {
+   return bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'john'; 
+
+function changeLeastFavoriteCustomer(newLeastFavoriteCustomer) {
+  leastFavoriteCustomer = newLeastFavoriteCustomer;
+}
